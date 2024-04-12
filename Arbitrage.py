eb6f9b1409b1b67d354392e60c372ee73a0aab64
SUN <100919359+tinyansun@users.noreply.github.com>
2024-04-12 15:36:42 +0800

    py

--- Arbitrage.py
+++ Arbitrage.py
@@ -22,31 +22,43 @@ def my_swap(input_token, output_token, num):
 
 # initial
 path = []
-path.append("B")
+path.append("tokenB")
 
 # B -> A
 out_1st = my_swap(10, 17, 5)
-path.append("A")
-# A -> E
-out_2nd = my_swap(21, 5, out_1st)
-path.append("E")
-# E -> D
-out_3rd = my_swap(25, 60, out_2nd)
-path.append("D")
-# D -> C
-out_4th = my_swap(12, 30, out_3rd)
-path.append("C")
-# C -> B
-out_5th = my_swap(4, 36, out_4th)
-path.append("B")
+path.append("tokenA")
+print(out_1st)
+# A -> C
+out_2nd = my_swap(11, 7, out_1st)
+path.append("tokenC")
+print(out_2nd)
+# C -> E
+out_3rd = my_swap(10, 8, out_2nd)
+path.append("tokenE")
+print(out_3rd)
 
+# E -> D
+out_4th = my_swap(25, 60, out_3rd)
+path.append("tokenD")
+print(out_4th)
+# D -> C
+out_5th = my_swap(12, 30, out_4th)
+path.append("tokenC")
+print(out_5th)
+# C -> B
+out_6th = my_swap(4, 36, out_5th)
+path.append("tokenB")
+print(out_6th)
 
 # print path
 print("path: ", end = '')
 for i in range(len(path)):
     if (i == len(path) - 1):
-        print("token", path[i], ",", end='')
+        print(path[i], end='')
+        print(",", end='')
     else:
-        print("token", path[i], "->", end='')
-print("tokenB balance=", out_5th)
+        print(path[i], end='')
+        print("->", end='')
+print("tokenB balance=", end='')
+print(out_6th, end='')
 print('\n')
